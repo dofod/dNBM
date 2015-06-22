4b568754e998d87c68475f930b9cae908341166b
Saurabh Wahile <saurabh-wahile@hotmail.com>
2015-06-22 23:20:01 +0530

    NodeJS

--- run.sh
+++
@@ -1,1 +0,0 @@
-python gossiper.py --id 5,192.168.1.5
---
+++ test2.js
@@ -0,0 +1,20 @@
+zmq = require('zmq');
+port = 'tcp://127.0.0.1:12345';
+var socket = zmq.socket('pub');
+
+  //socket.identity = 'publisher' + process.pid;
+  
+  var stocks = ['AAPL', 'GOOG', 'YHOO', 'MSFT', 'INTC'];
+
+  socket.bind(port, function(err) {
+    if (err) throw err;
+    console.log('bound!');
+    
+    setInterval(function() {
+      var symbol = stocks[Math.floor(Math.random()*stocks.length)]
+        , value = Math.random()*1000;
+
+      console.log(socket.identity + ': sent ' + symbol + ' ' + value);
+      socket.send(symbol + ' ' + value);
+    }, 100);
+  });
