9302055c4876ae8a2d8f6875fded98b495acc810
Saurabh Wahile <saurabh-wahile@hotmail.com>
2015-06-28 23:17:05 +0530

    tixati test

--- tixati_remote.js
+++ tixati_remote.js
@@ -3,48 +3,71 @@
  */
 var request = require('request');
 var cheerio = require('cheerio');
+var querystring = require('querystring');
 
-var host = 'localhost';
-var port = '8888';
-var username = 'user';
-var password = 'user';
-/*
-request(
-    {
-        url: url,
-        headers : {
-            //"Authorization" : auth,
-            "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
-        }
-    }, function(error, response, html) {
-    if (!error) {
-        console.log(url);
-        console.log(html);
-        var $ = cheerio.load(html);
-        console.log($('table[class=xferstable]').html());
-    }
-    else{
-        console.log('Error loading page '+url);
-    }
-});
-    */
+remote = new TixatiRemote(
+    '127.0.0.1',
+    '8888',
+    'user',
+    'user'
+);
 
-var digest = require('http-digest-client').createDigestClient(username, password, false);
-digest.request({
-    host: host,
-    path: '/transfers',
-    port: port,
-    method: 'GET'
-}, function (res) {
-    res.on('data', function (data) {
-        var $ = cheerio.load(data.toString());
-        $('.xferstable tr').each(function(day) {
-            $(this).find('td').each(function() {
-                console.log($(this).text());
+function TixatiRemote(host, port, username, password){
+    this.url = 'http://'+host+':'+port;
+    this.connection = require('http-digest-client')(host, port, username, password, false);
+    this.transfers = [];
+    var self = this;
+    this.printTransfers = function(){
+        console.log(self.transfers);
+    };
+
+    this.startTransfer = function(){
+        var postData = querystring.stringify({
+            start:'start'
+        });
+        self.connection.request({
+                path: self.transfers[0]+'/action',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Content-Length': Buffer.byteLength(postData)
+                },
+                postData:postData
+            },
+            function(res){
+                res.setEncoding('utf8');
+                res.on('data', function (chunk) {
+                    console.log('Response: ' + chunk);
+                });
+
             });
+        /*self.connection.on('end', function() {
+            console.log('end');
+        });*/
+        //self.connection.write(postData);
+        //self.connection.end();
+    };
+
+    this.connection.request({
+            path: '/transfers',
+            method: 'GET'
+    }, 
+    function (res) {
+        res.setEncoding();
+        var buffer="";
+        res.on('data', function (data) {
+            buffer += data;
+        });
+        res.on('end', function(){
+            var $ = cheerio.load(buffer);
+            $('.xferstable tr td a').each(function() {
+                self.transfers.push($(this).attr('href'));
+            });
+            self.printTransfers();
+            self.startTransfer();
+        });
+        res.on('error', function (err) {
+            console.log('Error loading');
         });
     });
-    res.on('error', function (err) {
-        console.log('Error loading page '+url);
-    });
-});
+}
